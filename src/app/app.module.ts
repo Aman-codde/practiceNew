68a03a347c6cf8f6158c42578026fe7ad4638ee4
Aman <amandeepk221@gmail.com>
2022-06-29 07:50:11 -0700

    chore: auto import board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-<<<<<<< HEAD
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-=======
->>>>>>> 26d2aa6296282a9a651413cfedd77fb2d64fd872
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
-=======
-    StoreModule.forRoot({}, {})
->>>>>>> 26d2aa6296282a9a651413cfedd77fb2d64fd872
   ],
   providers: [],
   bootstrap: [AppComponent]
