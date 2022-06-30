c0903baaa925a94945b9e5e28041908881e6ee02
Aman <amandeepk221@gmail.com>
2022-06-30 06:32:47 -0700

    feat: add players and turn properties

--- src/app/reducers/players.reducer.ts
+++ src/app/reducers/players.reducer.ts
@@ -4,11 +4,13 @@ import { Action, createReducer, on } from '@ngrx/store';
 export const playersFeatureKey = 'players';
 
 export interface State {
-
+  players: String[];
+  turn: 0 | 1;
 }
 
 export const initialState: State = {
-
+  players: ['O','X'],
+  turn: 0
 };
 
 export const reducer = createReducer(
--- src/app/selectors/players.selectors.ts
+++ src/app/selectors/players.selectors.ts
@@ -1,2 +1,16 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
+import * as fromPlayers from '../reducers/players.reducer';
+
+const selectPlayersFeatureState = createFeatureSelector<fromPlayers.State>('players');
+
+export const selectPlayers = createSelector(
+    selectPlayersFeatureState, 
+    state => state.players
+);
+
+export const selectTurn = createSelector(
+    selectPlayersFeatureState,
+    state => state.turn
+);
+
